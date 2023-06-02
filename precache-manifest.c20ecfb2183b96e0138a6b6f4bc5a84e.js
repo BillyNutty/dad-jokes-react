7a2fc901bd9aca6c3963eb5430a9c532c8cbea6f
William Notturno <williamnotturno@Williams-MBP.fios-router.home>
2023-06-02 17:25:22 -0400

    Updates

---
+++ precache-manifest.c20ecfb2183b96e0138a6b6f4bc5a84e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0915aad1fcd82a738f7e",
+    "url": "/dad-jokes-react/static/css/main.2d884deb.chunk.css"
+  },
+  {
+    "revision": "0915aad1fcd82a738f7e",
+    "url": "/dad-jokes-react/static/js/main.0915aad1.chunk.js"
+  },
+  {
+    "revision": "43a33ea9b5862b0054c4",
+    "url": "/dad-jokes-react/static/js/1.43a33ea9.chunk.js"
+  },
+  {
+    "revision": "6bcbd113212901990350",
+    "url": "/dad-jokes-react/static/js/runtime~main.6bcbd113.js"
+  },
+  {
+    "revision": "9232c7b36b0b34121522006570771703",
+    "url": "/dad-jokes-react/index.html"
+  }
+];
